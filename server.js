1d678ebf8495b8830a14c2e69d92a2484164b38c
AnKhang <minecraftcopyright1302@gmail.com>
2025-06-01 18:33:54 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const dotenv = require('dotenv');
+const connectDB = require('./src/config/db');
+const app = require('./src/app');
+
+dotenv.config();
+
+// Kết nối MongoDB trước khi server chạy
+connectDB().then(() => {
+
+    const PORT = process.env.PORT || 8001;
+
+    app.listen(PORT, () => {
+        console.log(`Server listening on http://localhost:${PORT}`);
+    });
+}).catch((err) => {
+    console.error('Failed to connect to MongoDB:', err);
+});
+
